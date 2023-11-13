e77395f41b155a95890a0d30af1fc9d2d40fef43
Steven Velasquez-Chanci <sjv5461@psu.edu>
2023-11-12 20:21:44 -0500

    Add unit test framework to use

--- include/Utilities/Math.hpp
+++ include/Utilities/Math.hpp
@@ -22,7 +22,7 @@ namespace Math {
      * This template function calculates the cubic roots of a cubic equation
      * in the standard form x^3 + ax^2 + bx + c, given coefficients a, b, and c.
      *
-     * @tparam PT Type of the coefficients (e.g., double, float).
+     * @tparam PT Type of the coefficients (e.g., long double, double, float).
      * @param a Coefficient of the quadratic term.
      * @param b Coefficient of the linear term.
      * @param c Constant term.
@@ -31,21 +31,23 @@ namespace Math {
     template<typename PT>
     constexpr std::vector<PT> cubicRootsFind(PT a, PT b, PT c) {
         // Calculate intermediate variables for cubic root calculation
-        auto Q = (std::pow(a, 2) - 3 * b) / 9;
-        auto R = (2 * std::pow(a, 3) - 9 * a * b + 27 * c) / 54;
+        PT Q = (std::pow(a, 2) - 3 * b) / 9;
+        PT R = (2 * std::pow(a, 3) - 9 * a * b + 27 * c) / 54;
 
-        auto Q3 = std::pow(Q, 3);
-        auto R2 = std::pow(R, 2);
+        PT Q3 = std::pow(Q, 3);
+        PT R2 = std::pow(R, 2);
 
-        auto M = R2 - Q3;
+        PT M = R2 - Q3;
 
         // Check conditions for different cases of cubic roots
         if (R2 > Q3) {
-            auto S = -std::copysign(1.0, R) * std::cbrt(std::abs(R) + std::sqrt(M));
-            auto T = S > 0.0 ? Q / S : 0;
+            // One root
+            PT S = -std::copysign(1.0, R) * std::cbrt(std::abs(R) + std::sqrt(M));
+            PT T = S > 0.0 ? Q / S : 0;
             return {S + T - a / 3};
         } else {
-            auto theta = std::acos(R / std::sqrt(Q3));
+            // Three roots
+            PT theta = std::acos(R / std::sqrt(Q3));
             return {
                 -(2 * std::sqrt(Q) * std::cos(theta / 3) - a / 3),
                 -(2 * std::sqrt(Q) * std::cos((theta + 2 * constants::pi()) / 3) - a / 3),
