7a1516f5f9d2ce61601076c6795ad1d6f37de4c0
Steven Velasquez-Chanci <sjv5461@psu.edu>
2023-11-23 17:44:28 -0500

    Add Non-Random Mixing Rule logic

--- include/PhaseBehavior/MixingRules.hpp
+++ include/PhaseBehavior/MixingRules.hpp
@@ -2,7 +2,10 @@
 #define MIXING_RULES_HPP
 
 #include <tuple>
+#include <map>
+#include <cmath>
 #include "Utilities/Types.hpp"
+#include "Component.hpp"
 #include "Mixture.hpp"
 
 
@@ -16,10 +19,40 @@ namespace PhaseBehavior::EoS::MixingRules{
     typename EoS
     >
     class NonRandomMixingRule{
-        
+    private:
+        using MapType = std::map<Component const&, NP_t>;
+
+        MapType componentAParameter, componentBParameter;
     public:
         std::tuple<NP_t, NP_t> operator() (Mixture const& mixture, NP_t const& pressure, NP_t const& temperature){
 
+            for(auto const& mixComponent : mixture){
+
+                componentAParameter[mixComponent.pure()] = attractionParameter()*
+                EoS::componentAttraction(mixComponent.pure(), pressure, temperature);
+
+                componentBParameter[mixComponent.pure()] = covolumeParameter()*
+                mixComponent.pure().reducedPressure(pressure)/mixComponent.pure().reducedTemperature(temperature);
+
+            }
+
+            NP_t mixtureAttraction = 0;
+            NP_t mixtureCovolume = 0;
+
+            for(auto const& mixComponentI : mixture){
+                for(auto const& mixComponentJ: mixture){
+
+                    auto binaryCoefficient = (1 - mixture.interactionCoefficient(mixComponentI, mixComponentJ))*
+                    std::sqrt(componentAParameter[mixComponentI.pure()]*componentAParameter[mixComponentJ.pure()]);
+
+                    mixtureAttraction += mixComponentI.composition()*mixComponentJ.composition()*binaryCoefficient;
+
+                }
+
+                mixtureCovolume += mixComponentI.composition()*componentBParameter[mixComponentI.pure()];
+            }
+
+            return {mixtureAttraction, mixtureCovolume};
         }
     
     };
