7b9c6228066bc2343f338d33f5f3fdf293e462aa
Steven Velasquez-Chanci <sjv5461@psu.edu>
2026-06-30 10:30:17 -0400

    Adjust lambda capture to copy by value by default

--- examples/SPE3TimeEnvelope.cpp
+++ examples/SPE3TimeEnvelope.cpp
@@ -48,7 +48,7 @@ void run_batched_unrolled_threads(F&& f) {
 int main(){
 
     constexpr int numberOfComponents = 6;
-    constexpr std::size_t NTHREADS = 16;
+    constexpr std::size_t NTHREADS = 32;
 
     PhaseBehavior::Component CO2   = {"CO2",      7384.3,    304.39,   2.1385799828e-3,  44.01,   0.2667};
     PhaseBehavior::Component C1    = {"CH4",      4594.7,    190.74,   6.1477929425e-3,  16.043,  0.0104};
