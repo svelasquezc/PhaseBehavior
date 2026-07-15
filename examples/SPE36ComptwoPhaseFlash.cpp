b2db0ded8248b9b4d8590cb36490fa3cabb03362
Steven Velásquez-Chancí <sjv5461@psu.edu>
2026-07-14 22:19:32 -0400

    Modify SPE3 dew point calculation to consider upper dew point and add SPE3 6 components flash for COMSOL

---
+++ examples/SPE36ComptwoPhaseFlash.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <tuple>
+#include <array>
+
+#include <PhaseBehavior/Component.hpp>
+#include <PhaseBehavior/Mixture.hpp>
+#include <PhaseBehavior/Phase.hpp>
+#include <PhaseBehavior/EquationsOfState.hpp>
+#include <PhaseBehavior/PhaseEquilibrium.hpp>
+
+using PR = PhaseBehavior::EoS::PR::PengRobinson;
+using SRK = PhaseBehavior::EoS::SRK::SoaveRedlichKwong;
+
+
+int main(){
+    
+    using PhaseBehavior::Component, PhaseBehavior::VaporLiquidEquilibrium::isothermalTwoPhaseFlash;
+    using PhaseBehavior::Component;
+
+    auto C1 = Component {"C1",      4599.2,    190.56,   6.1477929425e-3, 16.04,  0.0114};
+    auto C2 = Component {"C2",      4872.2,    305.32,   4.8402710552e-3, 30.07,  0.0990};
+    auto C3 = Component {"C3",      4251.2,    369.89,   4.5355587810e-3, 44.10, 0.1521};
+    auto C4 = Component {"C4",      3796.3,    425.34,   4.3890449440e-3, 58.12, 0.1995};
+    auto C5 = Component {"C5",      3368.8,    469.89,   4.3103448281e-3, 72.15,  0.2514};
+    auto C6 = Component {"C6",      3012.3,    507.56,   4.2885324643e-3, 86.18, 0.2994};
+
+    std::array<double, 6> composition = {0.6223, 0.1408, 0.0835, 0.0438, 0.0232, 0.0864};
+
+    PhaseBehavior::Mixture SPE3{{C1, composition[0]}, {C2, composition[1]}, {C3, composition[2]}, {C4, composition[3]}, {C5, composition[4]}, {C6, composition[5]}};
+
+
+    auto casePressure = 1.6e3 /*[kPa]*/;
+    //auto casePressure = 5.3e3 /*[kPa]*/;
+    //auto casePressure = 1.5e3 /*[kPa]*/;
+    auto caseTemperature = 350.0 /*[K]*/;
+
+
+    auto result = isothermalTwoPhaseFlash<PR>(SPE3, casePressure, caseTemperature);
+
+
+    if (result == PhaseBehavior::VaporLiquidEquilibrium::PhaseStabilityResult::Unstable){
+        auto liquidPhase = PhaseBehavior::Phase::LiquidLikePhase(SPE3);
+        liquidPhase.molarVolume(SPE3.compressibility(PhaseBehavior::PhaseName::liquid), casePressure, caseTemperature);
+        auto densityL = liquidPhase.density();
+        std::cout << "Liquid Density: "<< densityL << std::endl;
+        auto gasPhase = PhaseBehavior::Phase::VaporLikePhase(SPE3);
+        gasPhase.molarVolume(SPE3.compressibility(PhaseBehavior::PhaseName::vapor), casePressure, caseTemperature);
+        auto densityG = gasPhase.density();
+        std::cout << "Gas Density: "<< densityG << std::endl;
+        std::cout << "liquid Composition: (";
+        double pseudoMolarWL = 0.0;
+        for (auto c : SPE3){
+            std::cout << c.composition(PhaseBehavior::PhaseName::liquid)<<",";
+            pseudoMolarWL += c.composition(PhaseBehavior::PhaseName::liquid)*c->molarWeight()/1000;
+        }
+        std::cout << ")"<<std::endl << "gas Composition: (";
+        double pseudoMolarWG = 0.0;
+        for (auto c : SPE3){
+            std::cout << c.composition(PhaseBehavior::PhaseName::vapor)<<",";
+            pseudoMolarWG += c.composition(PhaseBehavior::PhaseName::vapor)*c->molarWeight()/1000;
+        }
+        std::cout <<")"<< std::endl;
+
+        auto gasMolarDensity = densityG/pseudoMolarWG;
+        auto liquidMolarDensity = densityL/pseudoMolarWL;
+
+        auto liquidVolumeFraction = 1/(1 + (SPE3.molarFraction(PhaseBehavior::PhaseName::vapor)/SPE3.molarFraction(PhaseBehavior::PhaseName::liquid))*(liquidMolarDensity/gasMolarDensity));
+    
+        double COMSOLInitialDensityL = 14000.0;
+        double COMSOLInitialDensityV = 100.0;
+
+
+        double COMSOLInitialFractionL = (liquidMolarDensity*liquidVolumeFraction + gasMolarDensity*(1-liquidVolumeFraction) - COMSOLInitialDensityV)/(COMSOLInitialDensityL - COMSOLInitialDensityV);
+
+
+        std::cout << "Gas Molar Density: " << gasMolarDensity << std::endl;
+        std::cout << "Liquid Molar Density: " << liquidMolarDensity << std::endl;
+        std::cout << "Liquid Volume Fraction: " << liquidVolumeFraction << std::endl;
+        std::cout << "COMSOL Initial Liquid Fraction: " << COMSOLInitialFractionL << std::endl;
+    };
+    return 0;
+}
--- examples/SPE3dewPressureVSTime.cpp
+++ examples/SPE3dewPressureVSTime.cpp
@@ -13,19 +13,30 @@
 
 #include <rapidcsv.h>
 
+using NP_t = PhaseBehavior::Types::NumericalPrecision;
+using PR = PhaseBehavior::EoS::PR::PengRobinson;
+
+
 int main(){
+
+    using PhaseBehavior::PhaseName;
+    using PhaseBehavior::Component;
+    using PhaseBehavior::Mixture;
+    using PhaseBehavior::VaporLiquidEquilibrium::PhaseStabilityResult;
+    using PhaseBehavior::VaporLiquidEquilibrium::phaseStability;
+
 
     constexpr double temperature = 350.0; // [K] Example temperature in Kelvin
 
-    auto C1 = PhaseBehavior::Component {"C1",      4599.2,    190.56,   6.1477929425e-3, 16.04,  0.0114};
-    auto C2 = PhaseBehavior::Component {"C2",      4872.2,    305.32,   4.8402710552e-3, 30.07,  0.0990};
-    auto C3 = PhaseBehavior::Component {"C3",      4251.2,    369.89,   4.5355587810e-3, 44.10, 0.1521};
-    auto C4 = PhaseBehavior::Component {"C4",      3796.3,    425.34,   4.3890449440e-3, 58.12, 0.1995};
-    auto C5 = PhaseBehavior::Component {"C5",      3368.8,    469.89,   4.3103448281e-3, 72.15,  0.2514};
-    auto C6 = PhaseBehavior::Component {"C6",      3012.3,    507.56,   4.2885324643e-3, 86.18, 0.2994};
+    auto C1 = Component {"C1",      4599.2,    190.56,   6.1477929425e-3, 16.04,  0.0114};
+    auto C2 = Component {"C2",      4872.2,    305.32,   4.8402710552e-3, 30.07,  0.0990};
+    auto C3 = Component {"C3",      4251.2,    369.89,   4.5355587810e-3, 44.10, 0.1521};
+    auto C4 = Component {"C4",      3796.3,    425.34,   4.3890449440e-3, 58.12, 0.1995};
+    auto C5 = Component {"C5",      3368.8,    469.89,   4.3103448281e-3, 72.15,  0.2514};
+    auto C6 = Component {"C6",      3012.3,    507.56,   4.2885324643e-3, 86.18, 0.2994};
 
     std::ofstream outputFile("dew_pressure_results.csv");
-    outputFile << "Time,DewPressure"<<std::endl;
+    outputFile << "Time,LowerDewPressure,UpperDewPressure"<<std::endl;
 
     rapidcsv::Document doc("SPE3_UpscaledProps_th90.csv");
 
@@ -38,10 +49,30 @@ int main(){
     std::vector<double> xC5 = doc.GetColumn<double>("C5 Composition (1)");
     std::vector<double> xC6 = doc.GetColumn<double>("C6 Composition (1)");
 
-
-    auto objectiveDewPoint = [vaporMolarFraction=1, temperature=temperature](PhaseBehavior::Mixture& mixture, NP_t const& pressure){
+    auto objectiveDewPoint = [temperature=temperature](Mixture& mixture, NP_t const& vaporMolarFraction, NP_t const& pressure){
                 NP_t sum = 0;
-                mixture.initializeEquilibriumCoefficients(pressure, temperature); //Wilson Coeff
+                for (auto& mixComp : mixture){
+                    const auto Ki = mixComp.equilibriumCoefficient();
+                    auto liquidComposition = mixComp.composition()/(1.0 + vaporMolarFraction*(Ki-1));
+                    auto vaporComposition  = Ki*liquidComposition;
+                    mixComp.composition(PhaseName::liquid, liquidComposition);
+                    mixComp.composition(PhaseName::vapor, vaporComposition);
+                }
+
+                PR vaporEoS, liquidEoS;
+                vaporEoS(mixture, pressure, temperature, "vapor");
+                mixture.compressibility("vapor", vaporEoS.selectedCompressibility());
+                vaporEoS.fugacities(mixture, "vapor");
+
+                liquidEoS(mixture, pressure, temperature, "liquid");
+                mixture.compressibility("liquid", liquidEoS.selectedCompressibility());
+                liquidEoS.fugacities(mixture, "liquid");
+
+                for (auto& mixComponent : mixture){
+                    auto newEquilibriumCoefficient = mixComponent.fugacityCoefficient(PhaseName::liquid)/mixComponent.fugacityCoefficient(PhaseName::vapor);
+                    mixComponent.equilibriumCoefficient(newEquilibriumCoefficient);
+                }
+
                 for (const auto& mixComp : mixture){
                     const auto Ki = mixComp.equilibriumCoefficient();
                     sum += mixComp.composition()*(Ki-1.0)/(1.0+vaporMolarFraction*(Ki-1.0));
@@ -49,21 +80,21 @@ int main(){
                 return sum;
             };
 
+    auto lowerDewPressure = 1.8e3; //[kPa]
+    auto upperDewPressure = 15e3; //[kPa]
+
     for(std::size_t i = 0; i < xC1.size(); ++i){
         PhaseBehavior::Mixture mixture {{C1, xC1[i]}, {C2, xC2[i]}, {C3, xC3[i]}, {C4, xC4[i]}, {C5, xC5[i]}, {C6, xC6[i]}};
 
-        auto initialGuessPressure = 1.3e3; //[kPa]
-
-        auto dewConvergence = PhaseBehavior::Math::NewtonRaphson(initialGuessPressure, static_cast<std::function<NP_t(NP_t)>>(std::bind(objectiveDewPoint, mixture, std::placeholders::_1)));
-
-        NP_t dewPressure = 0;
-        
-        if (dewConvergence){
-            dewPressure = dewConvergence.value();
-        }else{
-            throw std::runtime_error("Failed to converge on dew point");
+        while(phaseStability<PR>(mixture, lowerDewPressure, temperature) == PhaseStabilityResult::Unstable){    
+            lowerDewPressure-=1; //psia
         }
-        outputFile << time[i] << "," << dewPressure << std::endl;
+
+        while(phaseStability<PR>(mixture, upperDewPressure, temperature) == PhaseStabilityResult::Stable){    
+            upperDewPressure-=1; //[kPa]
+        }
+
+        outputFile << time[i] << "," << lowerDewPressure << "," << upperDewPressure << std::endl;
     }
 
     return 0;
